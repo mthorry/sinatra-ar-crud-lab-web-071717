663ae696c8d37013c998d0634f2c4b757bf96291
mthorry <mthorry@gmail.com>
2017-08-09 13:55:40 -0400

    done

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,7 +7,46 @@ class ApplicationController < Sinatra::Base
     set :views, 'app/views'
   end
 
-  get '/' do 
-    
+  get '/posts/new' do
+    erb :new
   end
+
+  post '/posts' do
+    @post = Post.create(params)
+
+    redirect '/posts'
+    # params => {"name"=>"matt", "content"=>"hello!"}
+  end
+
+  get '/posts' do
+    erb :index
+  end
+
+  get '/posts/:id/edit' do
+    @post = Post.find(params[:id])
+
+    erb :edit
+  end
+
+  delete '/posts/:id/delete' do
+    @post = Post.find(params[:id])
+    @post.destroy
+    erb :deleted
+  end
+
+  get '/posts/:id' do
+    @post = Post.find(params[:id])
+    erb :show
+  end
+
+  patch '/posts/:id' do
+    @post = Post.find(params[:id])
+    @post.name = params[:name]
+    @post.content = params[:content]
+    @post.save
+    erb :show
+  end
+
+
+
 end
---
+++ app/models/post.rb
@@ -0,0 +1,17 @@
+class Post < ActiveRecord::Base
+
+  # attr_reader :name, :content
+
+  # @@all_posts = []
+
+  # def initialize(params)
+  #   @name = params[:name]
+  #   @content = params[:content]
+  #   @@all_posts << self
+  # end
+
+  # def self.all
+  #   @@all_posts
+  # end
+
+end
